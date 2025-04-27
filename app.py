3a25f138d56382644cfb74af8737f43305018533
nishantprime <nishantkumarprime@gmail.com>
2025-04-28 01:57:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response
 import requests
 import os
+from urllib.parse import urlparse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
